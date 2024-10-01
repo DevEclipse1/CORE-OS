167bb2b9851a4874fa172a2cc7085ccc3a4290af
DevEclipse <98752478+DevEclipse1@users.noreply.github.com>
2024-10-01 20:48:16 +0200

    Delete src directory

--- src/kernel/libs/stdint.h
+++
@@ -1,6 +0,0 @@
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef signed char int8_t;
-typedef typeof(sizeof(0)) size_t;
-#define NULL ((void*)0)
--- src/kernel/stdint.h
+++
@@ -1,6 +0,0 @@
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef signed char int8;
-typedef typeof(sizeof(0)) size;
-#define NULL ((void*)0)
